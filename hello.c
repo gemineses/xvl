bd893dbbe80541676d3fd92ba9e7acab7ff5847c
Antonio Juarez <ajuarez@gcib.net>
2018-05-16 17:04:55 -0700

    added input on loop

--- hello.c
+++ hello.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+
 int main(){
-	printf("hello world \n");
+	int q = 1;
+	char master_order[100];
+	while(q==1){
+		printf("Hey! \n");
+		scanf("%s", master_order);
+		if(strcmp(master_order,"exit")){
+			printf("bye bye...");
+			q=0;
+		}
+	}
 }
